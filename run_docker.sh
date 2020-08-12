7735a4df3c5b854d839e20c2f29cade07ea2cc37
levan92 <lingevan0208@gmail.com>
2020-08-12 12:07:19 +0800

    docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,9 @@
+export WORKSPACE=$HOME/Workspace
+export DATA=/media/dh/HDD
+
 xhost +local:docker
-docker run -it -v $HOME/Workspace/:$HOME/Workspace \
--v /media/dh/HDD/:/media/dh/HDD/ \
+docker run -it -v $WORKSPACE:$WORKSPACE \
+-v $DATA:$DATA \
 --net=host --ipc host --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --env="QT_X11_NO_MITSHM=1" \
 --device=/dev/video0:/dev/video0 --device=/dev/video1:/dev/video1 \
-levan92/cv-suite
+levan92/det2
