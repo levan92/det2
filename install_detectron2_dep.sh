4ff4e837b22fcaf4b21aeacecd88c563d7d0908d
levan92 <lingevan0208@gmail.com>
2021-04-28 00:51:59 +0800

    install pycocotools dep

--- install_detectron2_dep.sh
+++ install_detectron2_dep.sh
@@ -2,3 +2,11 @@
 pip3 install \
     torch==1.7.0 \
     torchvision==0.8.1
+
+# DETECTRON2 DEPENDENCY: PYCOCOTOOLS 
+pip3 install cython
+git clone https://github.com/pdollar/coco \
+    && cd coco/PythonAPI \
+    && python3 setup.py build_ext install \
+    && cd ../.. \
+    && rm -rf coco
